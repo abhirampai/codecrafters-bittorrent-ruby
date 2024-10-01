01ef5ff7d6a0ab2497b145bc4c97a19249748239
Abhiram Pai <abhirampai1999@gmail.com>
2024-10-01 20:21:33 +0530

    Passing second stage

--- app/main.rb
+++ app/main.rb
@@ -1,27 +1,30 @@
+# frozen_string_literal: true
+
 require 'json'
 
 if ARGV.length < 2
-  puts "Usage: your_bittorrent.sh <command> <args>"
+  puts 'Usage: your_bittorrent.sh <command> <args>'
   exit(1)
 end
 
 def decode_bencode(bencoded_value)
   if bencoded_value[0].chr.match?(/\d/)
     first_colon = bencoded_value.index(':')
-    if first_colon.nil?
-      raise ArgumentError, "Invalid encoded value"
-    end
-    return bencoded_value[first_colon+1..-1]
+    raise ArgumentError, 'Invalid encoded value' if first_colon.nil?
+
+    bencoded_value[first_colon + 1..]
+  elsif bencoded_value.match?(/^i-?\d*e/)
+    bencoded_value.gsub(/[ie]/, '').to_i
   else
-    puts "Only strings are supported at the moment"
+    puts 'Only strings are supported at the moment'
     exit(1)
   end
 end
 
 command = ARGV[0]
 
-if command == "decode"
-    encoded_str = ARGV[1]
-    decoded_str = decode_bencode(encoded_str)
-    puts JSON.generate(decoded_str)
+if command == 'decode'
+  encoded_str = ARGV[1]
+  decoded_str = decode_bencode(encoded_str)
+  puts JSON.generate(decoded_str)
 end
